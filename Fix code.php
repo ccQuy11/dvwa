5005170e3187480f8a783321dadef32f9f5ffdcf
hasag1ta <quyt4762@gmail.com>
2025-05-29 00:44:18 +0700

    Add files via upload

---
+++ Fix code.php
@@ -0,0 +1,13 @@
+<?php
+
+// The page we wish to display
+$file = $_GET[ 'page' ];
+
+// Input validation
+$file = str_replace( array( "http://", "https://" ), "", $file );
+$file = str_replace( array( "../", "..\\" ), "", $file );
+
+?>
+
+
+
